91b2d68c3416ae95f5c8825dd92644b520d7936f
Aryan <aryanbiswari14@gmail.com>
2022-04-02 11:31:16 +0530

    shift some content to new file and try to reduce code of connect-mongo

--- connect-mongo.js
+++ connect-mongo.js
@@ -1,35 +1,8 @@
-const { MongoClient } = require("mongodb");
-const url = "mongodb://localhost:27017";
-const client = new MongoClient(url);
-const database = "e-com";
-
-async function getData()
-{
-    let result = await client.connect();
-    let db = result.db(database);
-    return db.collection('products');
-    // let res = await collection.find({}).toArray();
-    // console.log(res);
-}
-
-//get all data inside collection
-// getData().then((resp)=> {
-//     resp.find().toArray().then((products)=>{
-//         console.log(products);
-//     })
-// });
-
-//get single data inside collection
-// getData().then((resp)=> {
-//     resp.find({brands : "samsung"}).toArray().then((products)=>{
-//         console.log(products);
-//     })
-// })
-
 //modern format
+const getData = require('./mongodb');
 const main = async ()=> {
     let data = await getData();
-    data = await data.find({brands:"samsung"}).toArray();
+    data = await data.find().toArray();
     console.log(data);
 }
 
