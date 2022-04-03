a3be206f32ebd7e20cde5b7bdd16cfa8f4884feb
Aryan <aryanbiswari14@gmail.com>
2022-04-03 11:38:48 +0530

    get post put method in api completed basics

---
+++ api.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const getData = require('./mongodb');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    let data = await getData();
+    let result = await data.find().toArray();
+    console.log(result);
+    res.send(result);
+});
+
+app.post('/', async (req, res) => {
+    let data = await getData();
+    let result = await data.insert(req.body);
+    res.send(result);
+});
+
+app.put('/', async (req, res) =>{
+    let data = await getData();
+    let result = await data.updateOne(
+        {name :"redmi 100"},
+        {$set : req.body}
+    );
+    res.send(result);
+});
+
+app.listen(8080);
