71368e7617fb974ec801e81f325e20beb7888489
Aryan <aryanbiswari14@gmail.com>
2022-04-04 12:24:08 +0530

    learn how to make post api using mongoose and perform crud operations in mongoose

--- index.js
+++ index.js
@@ -1,24 +1,72 @@
-const express = require('express');
-const reqFilter = require('./middlewares')
-const app = express();
-const route = express.Router();
+const mongoose = require("mongoose");
 
-//app.use(reqFilter);
+// const main = async () => {
+//   await mongoose.connect("mongodb://localhost:27017/e-com");
+//   const productSchema = new mongoose.Schema({
+//     name: String,
+//     price: Number,
+//   });
+//   const productModel = new mongoose.model("products", productSchema);
+//   let data = new productModel({ name: "redmi mi 78", price: 3000000 }); //if we try to pass extra prarameter which is not included in the schema then mongoose will ignore it.
+//   let result = await data.save();
+//   console.log(result);
+// };
 
-route.use(reqFilter)
+// main();
 
-app.get('/', (req, res) => {
-    res.send("Welcome to Home page")
-})
+mongoose.connect('mongodb://localhost:27017/e-com');
 
-route.get('/user', (req, res)=>{
-    res.send('login page');
-})
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    brand: String,
+    category: String
 
-route.get('/about', (req, res)=>{
-    res.send('Welcome to About page');
-})
+});
 
-app.use('/',route);
+const saveInDB = async () =>{
+    const Product = mongoose.model('products', productSchema);
+    let data = new Product({
+        name: "max 100",
+        price: 200,
+        brand: 'maxx',
+        category: 'Mobile'
+    });
+    const result = await data.save();
+    console.log(result);
+}
 
-app.listen(5000);
+// saveInDB();
+
+const updateInDB = async () => {
+    const Product = mongoose.model('products', productSchema);
+    let data = await Product.updateOne(
+        {
+            name : "max 100"
+        },
+        {
+            $set : {
+                name : "max 1000",
+                price : 1000,
+            }
+        }
+    )
+    console.log(data);
+}
+
+//updateInDB();
+const deleteInDB = async ()=>{
+    const Product = mongoose.model('products', productSchema);
+    let data = await Product.deleteOne({name:'max 1000'})
+    console.log(data);
+}
+
+//deleteInDB();
+
+const readInDB = async () => {
+    const Product = mongoose.model('products', productSchema);
+    let data = await Product.find()
+    console.log(data);
+}
+
+//readInDB();
